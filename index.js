d74341e3add1bfeedefdf68eaf51a8f64415dfe0
JairoHernandez <jairomh@hotmail.com>
2017-11-15 16:43:58 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// app.get watches for incoming requests.
+// req is Object representing incoming request. res Object represents outgiong response.
+app.get('/', (req, res) => {
+    res.send({hi: 'there'}); // Sends JSON response.
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(5000);

9659650830d37d3206bfca2942691be265f7af6e
JairoHernandez <jairomh@hotmail.com>
2018-01-22 23:31:47 -0600

    Fields From Config

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,37 +2,23 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form'; // This reduxForm helper is what allows communication to Redux store.
 import SurveyField from './SurveyField';
+import _ from 'lodash';
+import { Link } from 'react-router-dom';
+
+const FIELDS = [ // list of distinct items.
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject'},
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails' }
+];
 
 class SurveyForm extends Component {
     renderFields() {
-        return (
-            <div>
-                <Field
-                    label="Survey Title" 
-                    type="text" 
-                    name="title" 
-                    component={SurveyField} 
-                />
-                <Field
-                    label="Subject Line" 
-                    type="text" 
-                    name="subject" 
-                    component={SurveyField} 
-                />
-                <Field
-                    label="Email Body" 
-                    type="text" 
-                    name="body" 
-                    component={SurveyField} 
-                />
-                <Field
-                    label="Recipient List" 
-                    type="text" 
-                    name="emails" 
-                    component={SurveyField} 
-                />
-            </div>
-        );
+        // return _.map(FIELDS, field => {
+            // return <Field component={SurveyField} type="text" label={field.label} name={field.name} />
+        return _.map(FIELDS, ({ label, name }) => { // ES6 destructuring. "Get me the label" and "Get me the name".
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+        });
     }
 
     render() {
@@ -43,7 +29,15 @@ class SurveyForm extends Component {
                 <form onSubmit={this.props.handleSubmit(values => console.log('handleSubmit ->', values))}>
                     {/*<Field type="text" name="surveyTitle" component="input" />*/}
                     {this.renderFields()}
-                    <button type="Submit">Submit</button>
+
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+
+                    <button type="submit" className="teal bth-flat right white-text">
+                        NEXT
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
